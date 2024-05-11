71e48a4ae47f8e5e77ecb21cdf2142e5057be25b
Sol <sol6801@gmail.com>
2024-05-11 16:11:51 -0300

    Add animation to burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,21 @@ const burger = document.querySelector('#burger')
 button.addEventListener('click', () =>{
     nav.classList.toggle('active')
 })
-button.addEventListener('click', () =>{
-    burger.classList.toggle('active')
+burger.addEventListener('click', () =>{
+    burger.classList.toggle('bars')
 })
+
+
+// const closure = () =>{
+//     if (burger.classList.contains('bars')){
+//         burger.classList.remove('bars')
+//     }else{
+//     burger.classList.toggle('bars')
+//     }
+//     }
+// }
+// button.addEventListener('click', (closure) =>{
+//     burger.classList.add('bars')
+// })
+
+
