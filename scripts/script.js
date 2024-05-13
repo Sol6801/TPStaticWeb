8f9c5c7983b157f4db35f85492ab783ade022de9
Sol <sol6801@gmail.com>
2024-05-13 20:10:23 -0300

    Add info cards function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 const button = document.querySelector('.burger') 
 const nav = document.querySelector('.nav')
 const burger = document.querySelector('#burger')
+const province = document.querySelectorAll('path')
+const art = document.querySelectorAll('.info-card')
+const cards = document.querySelectorAll('.card')
+const closes = document.querySelectorAll('.close')
+const map = document.querySelector('.map')
+const text = document.querySelector('.container')
 
 button.addEventListener('click', () =>{
     nav.classList.toggle('active')
@@ -9,6 +15,24 @@ burger.addEventListener('click', () =>{
     burger.classList.toggle('active')
 })
 
+province.forEach((path) => {
+path.addEventListener('click', () =>{
+    
+    map.classList.add('noclick');
+    art.forEach((article) => {
+        
+        if (article.classList.contains(path.id)){
+            article.classList.add('card');
+            article.classList.add('activate');   
+        }
+        closes.forEach((close) => {
+            close.addEventListener('click', () =>{
+                article.classList.remove('activate')
+                article.classList.remove('card');
+                map.classList.remove('noclick');
 
-
-
+            })
+        })
+    })
+})
+})
